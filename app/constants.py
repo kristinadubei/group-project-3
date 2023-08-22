c39de0444e0c03f596ce9e51749f003b3f433389
Elee-Saleem <131826046+Elee-Saleem@users.noreply.github.com>
2023-08-22 14:09:58 -0400

    30

--- app/constants.py
+++
@@ -1,8 +0,0 @@
-sequence_of_columns = [
-    "Year", "Seq", "Glide", "Disaster_Group", "Disaster_Subgroup", "Disaster_Type",
-    "Disaster_Subtype", "Country", "ISO", "Region", "Continent", "Origin", "Associated_Dis",
-    "Associated_Dis2", "Aid_Contribution", "Dis_Mag_Value", "Dis_Mag_Scale", "Latitude",
-    "Longitude", "Local_Time", "Start_Year", "Start_Month", "Start_Day", "End_Year",
-    "End_Month", "End_Day", "Total_Deaths", "No_Injured", "No_Affected", "No_Homeless",
-    "Total_Affected", "Insured_Damages_USD", "Total_Damages_USD", "CPI"
-]
