0021343e3c768197f889ac3ae9b68948e275ec9d
NifaCout <131629385+NifaCout@users.noreply.github.com>
2023-08-22 15:19:27 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var map = null;
-
 function mapping(data, year_para, disaster_type_para) {
     var filtered_by_year = []
     for (const item of data) {
@@ -8,7 +7,6 @@ function mapping(data, year_para, disaster_type_para) {
         }
     }
     console.log(filtered_by_year)
-
     var filtered_by_disaster = []
     for (const item of filtered_by_year){
         if(item.Disaster_Type == disaster_type_para){
@@ -16,49 +14,45 @@ function mapping(data, year_para, disaster_type_para) {
         }
     }
     console.log(filtered_by_disaster)
-    
-
     if (!map) {
         map = L.map('map').setView([0, 0], 2);
-        
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
     }
-
     for(const item of filtered_by_disaster){
         var color = ''
-
         if(disaster_type_para === 'Earthquake'){
             color = 'red';
         }
         if(disaster_type_para === 'Flood'){
             color = 'blue';
         }
-        if(disaster_type_para === 'Volcanic activity'){
+        if(disaster_type_para === 'landslide'){
             color = 'orange';
         }
-        
-
+        if(disaster_type_para === 'storm'){
+            color = 'purple';
+        }
+        if(disaster_type_para === 'Volcanic activity'){
+            color = 'pink';
+        }
         var marker = L.circleMarker([item.Latitude, item.Longitude], {
             radius: 2 * 5,
             fillColor: color,
-            color: 'white',
+            color: 'Yellow',
             weight: 1,
             opacity: 1,
             fillOpacity: 0.8
         }).addTo(map);
-        
         if(item.Total_Deaths == ""){
             marker.bindPopup(`Total Deaths: 0`);
         }
         else{
             marker.bindPopup(`Total Deaths: ${item.Total_Deaths}`);
         }
-        
     }
-
     return;
 }
 function destroyMap() {
@@ -66,19 +60,14 @@ function destroyMap() {
         map.eachLayer(function (layer) {
             map.removeLayer(layer);
         });
-        
         map.remove();
-        
         map = null;
     }
 }
-
 function World() {
     const yearSelect = document.getElementById('yearSelect');
     const disasterSelect = document.getElementById('disasterSelect');
-
     destroyMap();
-
     fetch('map_data.json')
     .then(response => response.json())
     .then(data => {
@@ -89,22 +78,16 @@ function World() {
     .catch(error => {
         console.error('Error fetching or processing the CSV file:', error);
     });
-    
 }
-
 $(document).ready(function() {
     let select = $("#yearSelect")
     for(var i=1970; i<2022; i++){
         let option = $(`<option value="${i}">${i}</option>`);
         select.append(option);
     }
-
     const yearSelect = document.getElementById('yearSelect');
     const disasterSelect = document.getElementById('disasterSelect');
-    
-
     World();
-
     yearSelect.addEventListener('change', World);
     disasterSelect.addEventListener('change', World);
 });
